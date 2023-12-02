a65faa53c91cedb9e90f4125087b0c3b6c454b3c
jaminhoood <princeobj5@gmail.com>
2023-12-02 04:01:27 +0100

    feat: follow user

--- src/pages/User/Profile/Component/ProfileDetailsSection.jsx
+++ src/pages/User/Profile/Component/ProfileDetailsSection.jsx
@@ -1,10 +1,33 @@
+import axios from "axios"
 import PropTypes from "prop-types"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { URL } from "../../../../url"
 
 const ProfileDetailsSection = ({ data }) => {
-	const { id, photo, username, posts, followers, following, isCurrentUser } = data
+	const { id, photo, username, posts, followers, following, user, isCurrentUser, fetchProfile } = data
+	const [followState, setFollowState] = useState(`Follow`)
+	const [btnClicked, setBtnClicked] = useState(false)
 
-	console.log(isCurrentUser)
+	useEffect(() => {
+		if (followers.includes(user._id)) {
+			setFollowState(`Unfollow`)
+		} else {
+			setFollowState(`Follow`)
+		}
+	}, [followers])
+
+	const handleFollow = async () => {
+		const postData = { followerId: user._id }
+
+		await axios
+			.post(`${URL}/api/users/${followState.toLowerCase()}/${id}`, postData, {
+				withCredentials: true,
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${JSON.parse(localStorage.getItem(`t`))}`,
+			})
+			.then(() => fetchProfile(id))
+	}
 
 	return (
 		<div className="profile-details__section">
@@ -18,8 +41,8 @@ const ProfileDetailsSection = ({ data }) => {
 				<h3 className="profile-details__title">@{username}</h3>
 				<div className="profile-details__content">
 					<span className="profile-details__info--box">{posts} Posts</span>
-					<span className="profile-details__info--box">{followers} Followers</span>
-					<span className="profile-details__info--box">{following} Following</span>
+					<span className="profile-details__info--box">{followers.length} Followers</span>
+					<span className="profile-details__info--box">{following.length} Following</span>
 				</div>
 				<div className="profile-details__cta">
 					{isCurrentUser ? (
@@ -27,24 +50,16 @@ const ProfileDetailsSection = ({ data }) => {
 							<button className="cta__btn">Edit Profile</button>
 						</Link>
 					) : (
-						<button className="cta__btn">Follow</button>
+						<button
+							className="cta__btn"
+							onClick={handleFollow}>
+							{followState}
+						</button>
 					)}
 				</div>
 			</div>
 		</div>
 	)
-}
-
-ProfileDetailsSection.propTypes = {
-	data: PropTypes.shape({
-		id: PropTypes.string,
-		photo: PropTypes.string,
-		username: PropTypes.string,
-		posts: PropTypes.number,
-		followers: PropTypes.number,
-		following: PropTypes.number,
-		isCurrentUser: PropTypes.bool,
-	}),
 }
 
 export default ProfileDetailsSection
