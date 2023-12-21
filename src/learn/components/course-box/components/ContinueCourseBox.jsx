491eecffc1425fd6a4e3b48e2774e17fa3f98fc3
jaminhoood <princeobj5@gmail.com>
2023-12-21 13:10:02 +0100

    feat: course card

--- src/learn/components/course-box/components/ContinueCourseBox.jsx
+++ src/learn/components/course-box/components/ContinueCourseBox.jsx
@@ -1,5 +1,38 @@
+import courseImg from "../../../assets/course-continue.png"
+import courseProfileImg from "../../../assets/course-profile.png"
+
 const ContinueCourseBox = () => {
-	return <div>ContinueCourseBox</div>
+	return (
+		<div className="course__box--continue">
+			<div className="course__box--continue__img">
+				<img
+					src={courseImg}
+					alt="Course Title"
+				/>
+			</div>
+			<div className="course__box--continue__info">
+				<span className="course__box--continue__category">Frontend</span>
+				<h3 className="course__box--continue__heading">Beginner’s Guide to becoming a professional frontend developer</h3>
+				<div className="course__box--continue__progress">
+					<div
+						className="course__box--continue__progress--line"
+						style={{ width: `70%` }}></div>
+				</div>
+				<div className="course__box--continue__profile">
+					<div className="course__box--continue__profile--img">
+						<img
+							src={courseProfileImg}
+							alt="Profile"
+						/>
+					</div>
+					<div className="course__box--continue__profile--info">
+						<h4 className="course__box--continue__profile--name">Prashant Kumar singh</h4>
+						<p className="course__box--continue__profile--role">software Developer</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export { ContinueCourseBox }
